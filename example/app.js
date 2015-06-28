c61c83ac0867d84b3260695b7947335cde984420
Emilio Rodriguez <emiliorodriguez@gmail.com>
2015-06-28 13:39:31 +0200

    example

--- example/app.js
+++ example/app.js
@@ -1,39 +1,6 @@
-// This is a test harness for your module
-// You should do something interesting in this harness
-// to test out the module and to provide instructions
-// to users on how to use it by example.
-
-
-// open a single window
-var win = Ti.UI.createWindow({
-	backgroundColor:'white'
-});
-var label = Ti.UI.createLabel();
-win.add(label);
-win.open();
-
-// TODO: write your module tests here
 var movtomp4 = require('com.emilioicai.movtomp4');
-Ti.API.info("module is => " + movtomp4);
-
-label.text = movtomp4.example();
-
-Ti.API.info("module exampleProp is => " + movtomp4.exampleProp);
-movtomp4.exampleProp = "This is a test value";
-
-if (Ti.Platform.name == "android") {
-	var proxy = movtomp4.createExample({
-		message: "Creating an example Proxy",
-		backgroundColor: "red",
-		width: 100,
-		height: 100,
-		top: 100,
-		left: 150
-	});
-
-	proxy.printMessage("Hello world!");
-	proxy.message = "Hi world!.  It's me again.";
-	proxy.printMessage("Hello world!");
-	win.add(proxy);
-}
-
+//[IMPORTANT] At the moment the module requires the file to have a '.mov' extension
+var filePath = '/var/mobile/Containers/Data/Application/DDA6SA66D53S/Documents/testMovie.mov';
+movtomp4.convert(filePath, function(result){
+	console.log(result.filePath);
+});
